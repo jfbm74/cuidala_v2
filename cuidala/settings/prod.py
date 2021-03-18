5c0c81aea3457ed70a3eeb0bbc53ec66b15c3878
root <root@prod-cuidalapp.febum.tech>
2021-03-18 03:54:43 +0000

    Deleting nginx logs

--- cuidala/settings/prod.py
+++ cuidala/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['143.198.64.166', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
